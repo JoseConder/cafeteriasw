3b6533d072330f1d2f84808565bd305a5a2dbfce
joseconde <a299506@uach.mx>
2024-05-26 19:11:50 -0600

    cambios en cocina

--- comida.py
+++ comida.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
 from dbconfig import *
 import customtkinter as ctk
 
@@ -65,45 +66,52 @@ def actualizar_producto():
     actualizar_lista_productos()
 
 def actualizar_lista_productos():
+    treeview_productos.delete(*treeview_productos.get_children())
     productos = productos_collection.find()
-    listbox_productos.delete(0, tk.END)
     for producto in productos:
-        listbox_productos.insert(tk.END, f"{producto['nombre']} - {producto['descripcion']} - ${producto['precio']} - {producto['tiempo_preparacion']} mins")
+        nombre = producto['nombre']
+        descripcion = producto['descripcion']
+        precio = producto['precio']
+        tiempo_preparacion = producto['tiempo_preparacion']
+        treeview_productos.insert("", "end", values=(nombre, descripcion, f"${precio}", f"{tiempo_preparacion} mins"))
 
 def limpiar_campos():
-    entry_nombre.delete(0, tk.END)
-    entry_descripcion.delete(0, tk.END)
-    entry_precio.delete(0, tk.END)
-    entry_tiempo_preparacion.delete(0, tk.END)
+    entry_nombre.delete(0, ctk.END)
+    entry_descripcion.delete(0, ctk.END)
+    entry_precio.delete(0, ctk.END)
+    entry_tiempo_preparacion.delete(0, ctk.END)
 
+# Configuración de la apariencia
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
 
+# Creación de la interfaz de usuario
 root = ctk.CTk()
 root.title("Sistema de Pedidos")
 
-frame_campos = tk.Frame(root)
+frame_campos = ctk.CTkFrame(root)
 frame_campos.pack(pady=10)
 
-tk.Label(frame_campos, text="Nombre:").grid(row=0, column=0, padx=5, pady=5)
-entry_nombre = tk.Entry(frame_campos, width=50)
-entry_nombre.grid(row=0, column=1, padx=5, pady=5)
+ctk.CTkLabel(frame_campos, text="Nombre:").grid(row=0, column=0, padx=5, pady=5)
+entry_nombre = ctk.CTkEntry(frame_campos, width=300)
+entry_nombre.grid(row=0, column=1, padx=10, pady=5)
 
-tk.Label(frame_campos, text="Descripción:").grid(row=1, column=0, padx=5, pady=5)
-entry_descripcion = tk.Entry(frame_campos, width=50)
+ctk.CTkLabel(frame_campos, text="Descripción:").grid(row=1, column=0, padx=5, pady=5)
+entry_descripcion = ctk.CTkEntry(frame_campos, width=300)
 entry_descripcion.grid(row=1, column=1, padx=5, pady=5)
 
-tk.Label(frame_campos, text="Precio ($):").grid(row=2, column=0, padx=5, pady=5)
-entry_precio = tk.Entry(frame_campos, width=50)
+ctk.CTkLabel(frame_campos, text="Precio ($):").grid(row=2, column=0, padx=5, pady=5)
+entry_precio = ctk.CTkEntry(frame_campos, width=300)
 entry_precio.grid(row=2, column=1, padx=5, pady=5)
 
-tk.Label(frame_campos, text="Tiempo de Preparación (minutos):").grid(row=3, column=0, padx=5, pady=5)
-entry_tiempo_preparacion = tk.Entry(frame_campos, width=50)
+ctk.CTkLabel(frame_campos, text="Tiempo de Preparación (minutos):").grid(row=3, column=0, padx=5, pady=5)
+entry_tiempo_preparacion = ctk.CTkEntry(frame_campos, width=300)
 entry_tiempo_preparacion.grid(row=3, column=1, padx=5, pady=5)
 
 ctk.CTkButton(root, text="Agregar Producto", command=agregar_producto).pack()
+ctk.CTkButton(root, text="Actualizar Producto", command=actualizar_producto).pack()
 
-frame_productos = tk.Frame(root)
+frame_productos = ctk.CTkFrame(root)
 frame_productos.pack(pady=10)
 
 ctk.CTkLabel(frame_productos, text="Lista de Productos:").pack()
@@ -113,6 +121,8 @@ treeview_productos = ttk.Treeview(frame_productos, columns=columns, show="headin
 for col in columns:
     treeview_productos.heading(col, text=col)
 treeview_productos.pack()
+ctk.CTkButton(root, text="Eliminar Producto", fg_color=("red"),command=eliminar_producto).pack()
+
 
 actualizar_lista_productos()
 
